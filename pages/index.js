4f3315e0cbc980fcdd6d0122bbe5fb8cd384abef
Priscila Zeferino <priscila.zeferino23@gmail.com>
2025-02-08 16:44:29 -0300

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Sorria, voce esta sendo observado! Não por mim, pelos outros sites.</h1>
+    return <h1>Eu tenho um conto original publicado... 🤫 </h1>
 }
 
 export default Home;
